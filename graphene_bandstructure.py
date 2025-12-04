775df1ecebf90869084a891ad8cfa4443835f61d
JuKoWi <julius.willrich@uni-jena.de>
2025-12-04 18:09:18 +0100

    bandstructure corrected

--- graphene_bandstructure.py
+++ graphene_bandstructure.py
@@ -14,12 +14,12 @@ MoS2_labelToK = { 'G' : np.array([0, 0, 0]),
              'M' : np.array([0.5, 0, 0]),
              'K' : np.array([1/3, 1/3, 0]),
             }
-segments, labels = parsePath("GMKG", lattice, labelToK=MoS2_labelToK)
+segments, labels = parsePath("GKMG", lattice, labelToK=MoS2_labelToK)
 fig, ax = plt.subplots(1, 1, figsize=(8, 6))
 scale = 800
 
-Hk = w90.Hk(cells=cells, H=Hr, kFrac=[0,0,0]) 
-Sk = w90.Hk(cells=cells, H=Sr, kFrac=[0,0,0]) 
+Hk = w90.Hk(cells=cells, H=Hr, kFrac=[0.0, 0.0, 0.0])
+Sk = w90.Hk(cells=cells, H=Sr, kFrac=[0.0, 0.0, 0.0]) 
 vals, vecs = sp.linalg.eig(Hk, Sk)
 print(vals)
 
@@ -28,17 +28,16 @@ for i, (kPoints, relPos) in enumerate(segments):
     Sk = w90.Hk(cells=cells, H=Sr, kFrac=kPoints)
     vals, vecs = sp.linalg.eig(Hk, Sk)
     ax.plot(relPos, vals, '.', color='orange', ms=1)
-    print(np.shape(kPoints))
-    print(np.shape(relPos))
 fig.tight_layout()
 l, pos = zip(*labels)
-plotLines(ax=ax, pos=pos, labels=labels)
+point_symbols = []
+for i in labels:
+    point_symbols.append(i[0])
+plotLines(ax=ax, pos=pos, labels=point_symbols)
 plt.show()
 
 
-
-# vals, vr = sp.linalg.eig(Hgamma, Sgamma)
-# kFrac = [0,0,0]
-# Hgamma = w90.Hk(cells=cells, H=Hr, kFrac=kFrac)
-# Sgamma = w90.Hk(cells=cells, H=Sr, kFrac=kFrac)
-# Dgamma = w90.Dk(cells=cells, D=Rr, kFrac=kFrac)
+def parse_dftb_band(filepath):
+    with open(file=filepath, mode='r') as f:
+        for line in f.readlines():
+            
